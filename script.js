781f363dff99375448576e88787443b899008d73
emir <emirozger@gmail.com>
2023-09-08 19:38:50 +0300

    dark mode cookie

--- script.js
+++ script.js
@@ -2,23 +2,64 @@ function darkMode() {
   let element = document.body;
   let content = document.getElementById("darkModeToggle");
   element.className = "dark-mode";
-  content.innerText = "Dark Mode is ON";
+  content.checked = true;
+
 }
+
 function lightMode() {
   let element = document.body;
   let content = document.getElementById("darkModeToggle");
   element.className = "light-mode";
-  content.innerText = "Dark Mode is OFF";
+  content.checked = false;
+
 }
+
 const darkModeToggle = document.getElementById("darkModeToggle");
 
 function toggleDarkMode() {
   if (darkModeToggle.checked) {
-    darkMode();
+      darkMode();
   } else {
-    lightMode();
+      lightMode();
   }
-
 }
 
 darkModeToggle.addEventListener("change", toggleDarkMode);
+
+
+darkMode();
+
+function saveDarkModePreference(isDarkMode) {
+  document.cookie = `darkMode=${isDarkMode}; expires=Fri, 31 Dec 9999 23:59:59 GMT`;
+}
+
+function loadDarkModePreference() {
+  const cookies = document.cookie.split('; ');
+  for (const cookie of cookies) {
+      const [name, value] = cookie.split('=');
+      if (name === 'darkMode') {
+          return value === 'true';
+      }
+  }
+  return false;
+}
+
+
+const isDarkModeEnabled = loadDarkModePreference();
+if (isDarkModeEnabled) {
+  darkMode();
+} else {
+  lightMode();
+}
+
+
+darkModeToggle.addEventListener("change", function () {
+  if (darkModeToggle.checked) {
+      darkMode();
+      saveDarkModePreference(true);
+  } else {
+      lightMode();
+      saveDarkModePreference(false);
+  }
+});
+
